76c665e33b97ab3a446080d37ff13314de2b49be
flillo <63056403+lillof@users.noreply.github.com>
2023-09-28 01:04:18 -0300

    Create hardwarewIn.ino
    development skeleton (framework)
    - button0
    - button1
    -serial_lecture
    
    disclaimer: variables and timers are not declared in main yet

---
+++ proyect1/hardwarewIn.ino
@@ -0,0 +1,124 @@
+
+void hardwarewIn(){
+  boton_0();
+  boton_1();
+  Serial_lecture();
+}
+
+
+
+/////////////////////////////////////////
+// Activacion de boton 0 (push button) //
+/////////////////////////////////////////
+
+void boton_0{
+  switch(State_button_0){
+    
+    /////////////////////////////
+    // E0: WAIT FOR ACTIVATION //
+    ////////////////////////////
+    
+    case 0:
+      flag_button_0 = 0;
+      
+    break;
+
+    //////////////////////////////////////////////////////
+    // E1: WAIT FOR REAL ACTIVATION (spring phenomenon) //
+    /////////////////////////////////////////////////////
+    
+    case 1:
+
+    break;
+
+    /////////////////
+    // E2: FLAG UP //
+    /////////////////
+    
+    case 2:
+        flag_button_0 = 1;
+    break;
+    
+    ////////////////////////////////
+    // E3: WAIT FOR DE-ACTIVATION //
+    ////////////////////////////////
+    case 3:
+      
+    break;
+
+    /////////////////////////////////////
+    // E4: WAIT FOR REAL DE-ACTIVATION //
+    /////////////////////////////////////
+    case 4:
+      State_button_0 = 0
+    break;
+
+    
+    default:
+    State_button_0 = 0;
+    break;
+    
+  }
+
+
+  
+}
+
+
+
+////////////////////////////////////////////
+// Activacion de boton 1 (switch button) //
+///////////////////////////////////////////
+
+void boton_1{
+
+  switch(State_button_1){
+
+    /////////////////////////////
+    // E0: WAIT FOR ACTIVATION //
+    /////////////////////////////
+    case 0:
+
+    break;
+
+    //////////////////////////////////////////////////////
+    // E1: WAIT FOR REAL ACTIVATION (spring phenomenon) //
+    //////////////////////////////////////////////////////
+    
+    case 1:
+
+    break;
+    
+    /////////////////
+    // E2: FLAG UP //
+    /////////////////
+    
+    case 2:
+      
+      flag_button_0 = 1;
+      State_button_1 = 0;
+      
+    break;
+
+    default:
+    State_button_1 = 0;
+    break;
+
+    
+  }
+}
+
+
+
+////////////////////
+// Serial lecture //
+////////////////////
+
+void serial_lecture(){
+
+
+
+
+
+  
+}
