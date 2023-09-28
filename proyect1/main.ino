479b36c85290795af9892e00dbded69f8a916e46
flillo <63056403+lillof@users.noreply.github.com>
2023-09-28 00:45:52 -0300

    Update main.ino
    ADD 
    - PIN declaration and mode
    - Serial init for slave comms

--- proyect1/main.ino
+++ proyect1/main.ino
@@ -2,9 +2,23 @@
 // SUPERLOOP //
 ///////////////
 
-unsigned long startTime = 0;        // TIEMPO DE INICIO DEL LOOP
-unsigned long elapsedTime = 0;      // TIEMPO DE LOOP
+unsigned long startTime = 0;                // TIEMPO DE INICIO DEL LOOP
+unsigned long elapsedTime = 0;              // TIEMPO DE LOOP
 const int superloop = 10;  		    // Tiempo de superloop
+
+
+
+//////////
+// PINS //
+//////////
+
+const int boton_0= 1;	              // BOTON DE ENTRADA
+const int boton_1= 2;	              // BOTON DE ENTRADA
+
+const int led_0	= 3;	              // LED DE ENCENDIDO
+const int medicion_0 = 4              // Lectura analogica
+
+
 
 ///////////
 // SETUP //
@@ -12,11 +26,7 @@ const int superloop = 10;  		    // Tiempo de superloop
 
 void setup(){
 	
-  //////////////////////////
-	// HARDWARE DECLARATION //
-	//////////////////////////
-	
-	analogReadResolution(12);         // CAMBIAMOS RESOLUCION DE LECTURA ANALOGICA
+  analogReadResolution(12);         // CAMBIAMOS RESOLUCION DE LECTURA ANALOGICA
 
   /////////////////////////
   // CLOCK MODIFICATIONS //          MODIFICACION DE CICLOS DE RELOJ (OPCION PARA AHORRAR BATERIA(TENER EN CUENTA QUE ESTO MODIFICA LA TASA DE BAUDIOS PARA LA LECTURA DE PUERTO SERIAL))
@@ -31,12 +41,23 @@ void setup(){
   // SERIAL //
   ////////////
 	
-  Serial.begin(9600);                 // INICIAMOS SERIAL 1
+  Serial.begin(9600);                 // INICIAMOS SERIAL USB-PC
+  Serial1.begin(115200);               // INICIAMOS SERIAL MASTER_SLAVE	
+
+/////////////////////
+// PIN DECLARATION //
+/////////////////////
+
+pinMode(boton_0,	INPUT);		// PIN DE BOTON_0
+pinMode(boton_1,	INPUT);		// PIN DE BOTON_1
+pinMode(led_0,		OUTPUT);	// PIN DE LED_0 
+pinMode(medicion_0,	INPUT);		// PIN DE LECTURA 
+	
 }
 void loop(){
-	startTime = millis();  						  // INICIO DE SUPERLOOP
+  startTime = millis();               // INICIO DE SUPERLOOP
 	
-	hardwarewIn();                      // ENTRADA DE SEÑALES
+  hardwarewIn();                      // ENTRADA DE SEÑALES
   linkIn();                           // CAPA DE ENLACE PARA DATOS DE ENTRADA
   App();                              // CAPA DE APLICACION
   linkOut();                          // CAPA DE ENLACE PARA DATOS DE SALIDA
